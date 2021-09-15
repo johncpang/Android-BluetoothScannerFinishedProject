b0cafe0da3bae64805dfbbbc29eb6798fbba2092
John <john@brewingapps.com>
2021-09-15 21:16:46 +0800

    Fix missing imports

--- BluetoothScanner/app/src/main/java/com/tutsplus/matt/bluetoothscanner/DeviceListFragment.java
+++ BluetoothScanner/app/src/main/java/com/tutsplus/matt/bluetoothscanner/DeviceListFragment.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
